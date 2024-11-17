e63711148a74d0046792c8b8203dd09f84a9101b
sonugupta <sonugupta27032001@gmail.com>
2024-11-18 02:15:20 +0530

    started the project from here

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("node:http");
-const server = http.createServer(function(req,res){
-    if(req.url === "/getSecretData"){
-        res.end("There is no secrete data")
-    }
-    res.end("hello world")
-});
-server.listen(3333);
-//this new changes is made here 

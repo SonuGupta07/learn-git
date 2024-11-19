c8e51450c1801257d83fab31d458174a4abf2d81
sonugupta <sonugupta27032001@gmail.com>
2024-11-19 08:20:38 +0530

    handling error in exprees

--- src/app.js
+++ src/app.js
@@ -7,9 +7,32 @@ const {adminAuth,userAdmin}=require("./middlewares/auth");
 
 
 
-//third way of writing midllewares
-app.get("/user",userAdmin,(req,res)=>{
-    res.send("User data is send ");
+//1st way of handling error 
+app.use("/users",(req,res)=>{
+    throw new error("there is an error occured")
+    res.send("user is get logged");
+})
+app.use("/",(err,req,res,next)=>{// if you are using this always keep it to the last
+    if(err){
+        console.log(err.message)
+        res.status(501).send("something went wrong")
+    }
+})
+// if two is there  first one req and second one is res 
+// if there is three first one is req , res and third one is next 
+// if there is four firs one is err , res, req, and the last one is next
+//2nd way of handling error using try and catch
+app.use("/admin",(req,res)=>{
+    try{
+        throw new error ("there is an error");
+        res.send("user is getting loged");
+
+    }
+    catch(err){
+        res.send("second way of handling error");
+
+    }
+
 })
 
 

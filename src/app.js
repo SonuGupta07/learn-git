ef8a85bfedf4816b214b42b3c81b2335ea19808b
sonugupta <sonugupta27032001@gmail.com>
2025-01-03 12:02:06 +0530

    hi this is playing with route

--- src/app.js
+++ src/app.js
@@ -6,9 +6,29 @@ const app = express();
 //     res.send("hello from the server")
 // }) if we comment this then the downside code will run
 //it will only handle for /test
-app.use("/test",(req,res)=>{
-    res.send("this is testing")
+//playing with route 
+app.use("/ab?c",(req,res)=>{
+    res.send("Playing with route 1");
 })
+
+
+app.use("/ab+c",(req,res)=>{
+    res.send("playing with route 2");
+})
+app.use("/ab*cd",(req,res)=>{
+    res.send("playing with route 3");
+})
+app.use("/a(bc)?d",(req,res)=>{
+    res.send("playing with route 4");
+})
+app.get("/hello",(req,res)=>{
+    res.send("this is get request ")
+})
+
+app.use((req,res)=>{
+    res.send("this is dashboard")
+})
+
 app.listen(3000,()=>{
     console.log("server is listen at port 3000")
 });
